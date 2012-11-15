c852d0b72272700cef25d51550d01440e940a5a1
Rafi Romero <rromero@fb.com>
2012-11-14 23:05:48 -0800

    start of game

---
+++ game.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+class Ball:
+    def __init__(self, positionX, positionY, direction, speed):
+        self.px = positionX; # x coordinate (in pixels)
+        self.py = positionY; # y coordinate (in pixels)
+        self.dir = direction; # ball direction (in degrees)
+        self.vel = speed; # ball velocity (in pixels per second)
+
+class
